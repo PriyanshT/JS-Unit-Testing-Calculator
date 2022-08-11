f92be2d84142fafb314036d6e241696e9748d22a
Priyansh Thakar <priyanshthakar@gmail.com>
2022-08-11 16:40:42 -0400

    Updated test.js

--- test.js
+++ test.js
@@ -1,40 +1,40 @@
 const operations = require('./operations.js')
 const assert = require('assert');
 
-// it('should return true', () => {
+// it('unit testing tutorial working', () => {
 //     assert.equal(true, true)
 // })
 
 // checking if correctly calculates numbers
-it('correctly calculates the sum of 1 and 3', () => {
-    assert.equal(operations.add(1, 3), 4)
+it('Test is correctly calculating the sum of 3 and 5', () => {
+    assert.equal(operations.add(3, 5), 8)
 })
 
-it('correctly calculates the sum of -1 and -1', () => {
-    assert.equal(operations.add(-1, -1), -2)
+it('Test is correctly calculating the sum of -13 and -5', () => {
+    assert.equal(operations.add(-13, -5), -18)
 })
 
-it('correctly calculates the difference of 33 and 3', () => {
-    assert.equal(operations.subtract(33, 3), 30)
+it('Test is correctly calculating the difference of 26 and 13', () => {
+    assert.equal(operations.subtract(26, 13), 13)
 })
 
-it('correctly calculates the product of 12 and 12', () => {
-    assert.equal(operations.multiply(12, 12), 144)
+it('Test is correctly calculating the product of 18 and 12', () => {
+    assert.equal(operations.multiply(18, 12), 216)
 })
 
-it('correctly calculates the quotient of 10 and 2', () => {
-    assert.equal(operations.divide(10, 2), 5)
+it('Test is correctly calculating the quotient of 25 and 5', () => {
+    assert.equal(operations.divide(25, 5), 5)
 })
 
 // adding validations for non-number inputs'
-it('indicates failure when a string is used instead of a number', () => {
-    assert.equal(operations.validateNumbers('sammy', 5), false)
+it('Test is indicating failure when a string is used instead of a number', () => {
+    assert.equal(operations.validateNumbers('krishi', 5), false)
 })
 
-it('indicates failure when two strings is used instead of numbers', () => {
-    assert.equal(operations.validateNumbers('sammy', 'sammy'), false)
+it('Test is indicating failure when a string is used instead of a number', () => {
+    assert.equal(operations.validateNumbers('krishi', 'priyansh'), false)
 })
 
-it('successfully runs when two numbers are used', () => {
-    assert.equal(operations.validateNumbers(5, 5), true)
+it('Test is successfully running when two numbers are used', () => {
+    assert.equal(operations.validateNumbers(10, 5), true)
 })
